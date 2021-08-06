57a9d53146cd73dd25374d64ed167a66d85eb03f
AnirudhVasudev47 <69135996+AnirudhVasudev47@users.noreply.github.com>
2021-08-06 23:44:40 +0530

    Safe commit

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 //My routes
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/user")
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
 
 //DB Connection
 mongoose.connect(process.env.DATABASE, {
@@ -24,8 +25,9 @@ app.use(cookieParser());
 app.use(cors());
 
 //My Routes
-app.use("/api", authRoute);
-app.use("/api", userRoute);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
